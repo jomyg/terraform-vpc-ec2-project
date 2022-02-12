322ffaede0a12db316bb4d8760b1e3ab84b5bd48
Jomy george <87518944+jomyg@users.noreply.github.com>
2022-02-13 02:29:36 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {}
+
+variable "access_key"{}
+
+variable "secret_key"{}
+
+variable "project_name" {}
+
+variable "vpc_cidr" {}	
+
+variable "subnet_bit" {}
+
+variable "ami" {}
+
+variable "type" {}
